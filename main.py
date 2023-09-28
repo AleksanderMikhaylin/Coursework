4db11cd6d1f6ed5ccb834597f703f2c5e4aca532
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2023-09-29 09:17:50 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import json
 import requests
 from tqdm import tqdm
 from pprint import pprint
+from datetime import datetime
 
 class VK:
 
-   def __init__(self, access_token, user_id, version='5.131'):
+    def __init__(self, access_token, user_id, version='5.131'):
        self.token = access_token
        self.id = user_id
        self.version = version
        self.params = {'access_token': self.token, 'v': self.version}
 
-   def users_info(self):
+    def users_info(self):
        url = 'https://api.vk.com/method/users.get'
        params = {'user_ids': self.id}
        response = requests.get(url, params={**self.params, **params})
        response_json = response.json()
        error = response_json.get('error', '')
        if type(error) == dict:
-           return error.get('error_msg', 'Ошибка авторизации')
-       return response.json()
+           print(error.get('error_msg', 'Ошибка авторизации'))
+           exit()
 
-   def get_list_url_foto(self, max_foto=5):
+       if len(response_json.get('response', [])) == 0:
+           print(f'Пользователь с ID {user_id} не найден')
+           exit()
+
+    def get_list_url_foto(self, max_foto=5):
        url = 'https://api.vk.com/method/photos.get'
        params = {'extended': 1, 'album_id': 'wall', 'owner_id': self.id}
        response = requests.get(url, params={**self.params, **params})
@@ -29,20 +34,30 @@ class VK:
        response_foto = response.json()
        if response_foto.get('response', '') == '':
            if type(response_foto.get('error', '')) == dict:
-               return response_foto['error'].get('error_msg', 'Не известная ошибка')
-           return 'Не известная ошибка'
+               print(response_foto['error'].get('error_msg', 'Не известная ошибка'))
+           else:
+               print('Не известная ошибка')
+           exit()
 
        data_foto = response_foto['response']
 
        max_items = data_foto['items'][:max_foto]
 
+       list_file_name = []
        list_url = []
        for item in tqdm(max_items, desc = 'Получение списка фотографий с VK'):
            sizes = sorted(item['sizes'], key=lambda i: (i['height'], i['width']), reverse=True)
-           sizes[0]['url'].split('?')[0].split('.')
            attr_list = sizes[0]["url"].split("?")[0].split(".")
            exp = attr_list[len(attr_list) - 1]
-           list_url.append({'url': sizes[0].get('url', ''), 'size': sizes[0].get('type', ''), 'file_name': f'VK_ID_{self.id}_{max_items.index(item)}_{item.get("likes", {}).get("count", 0)}.{exp}'})
+           date_file = datetime.utcfromtimestamp(item['date']).strftime('%Y-%m-%d %H:%M:%S')
+           file_name = f'{item.get("likes", {}).get("count", 0)}'
+           if file_name in list_file_name:
+               file_name = file_name + '_' + date_file
+           else:
+               list_file_name.append(file_name)
+
+           file_name = f'{file_name}.{exp}'
+           list_url.append({'url': sizes[0].get('url', ''), 'size': sizes[0].get('type', ''), 'file_name': file_name})
 
        return list_url
 
@@ -71,6 +86,7 @@ class YD:
                 break
 
         if not is_folder:
+            params['path'] = folder_name
             response = requests.put(self.base_url + '/v1/disk/resources', headers=headers, params=params)
 
             if not response.status_code == 201:
@@ -89,31 +105,22 @@ class YD:
         return response_list
 
 if __name__ == '__main__':
+
     access_token_VK = ''
     user_id = input('Введите ID пользователя VK: ')
     access_token_YD = input('Введите токен с Полигона Яндекс.Диска: ')
 
     vk = VK(access_token_VK, user_id)
-    user_data = vk.users_info()
-
-    if type(user_data) == str:
-        print(user_data)
-        exit()
-    elif len(user_data.get('response', [])) == 0:
-        print(f'Пользователь с ID {user_id} не найден')
-        exit()
+    vk.users_info()
 
     # Получим список словарей фоток: (file_name, size, url)
     list_url_foto = vk.get_list_url_foto()
-    if not type(list_url_foto) == list:
-        print(list_url_foto)
-        exit()
 
     yd = YD()
     response_list = yd.upload_file_list(access_token_YD, 'Image_VK', list_url_foto)
 
     if type(response_list) == list:
-        with open('requiremеnts.txt', 'w') as f:
+        with open('requiremеnts.json', 'w') as f:
             f.writelines(json.dumps(response_list))
             f.close()
     else:
